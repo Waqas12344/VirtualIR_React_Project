3fc61f6a94106a1906e0fcd328ca81c6000f531d
react-redux-project <wm635950@gmail.com>
2025-01-28 11:18:57 +0500

    complete herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
 const HeroSection = () => {
   return (
     <div className='flex flex-col items-center mt-6 lg:mt-20'>
@@ -16,6 +17,14 @@ const HeroSection = () => {
             <a href="#" className='py-3 px-4 mx-3 border rounded-md'>
                 Documentation
             </a>
+        </div>
+        <div className="flex flex-col items-center  md:flex-row mt-10 justify-center">
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' >
+            <source src={video1} type='video/mp4'/>
+            </video>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4' >
+            <source src={video2} type='video/mp4'/>
+            </video>
         </div>
     </div>
   )
